881bba2ae6a796cd5ad7d2886b7ef9c9d3209026
Deepanshu Batra <80679706+Deepanshu979@users.noreply.github.com>
2023-03-30 06:49:58 +0530

    alert again changed

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -7,7 +7,7 @@ function Alert(props) {
   }
   return (
     props.alert && <div className={`alert alert-${props.alert.type}`} role="alert">
-        <strong>{capitalize(props.alert.type)}: {props.alert.msg}</strong> ok done done
+        <strong>{capitalize(props.alert.type)}: {props.alert.msg}</strong> 
     </div>
   )
 }
